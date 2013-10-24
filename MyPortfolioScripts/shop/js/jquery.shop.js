3cffb3cc4b448397199d741e8518e451fdd81eb0
PolyakovV <Valik@nm.ru>
2013-10-24 10:24:49 +0300

    Update

---
+++ MyPortfolioScripts/shop/js/jquery.shop.js
@@ -0,0 +1,25 @@
+
+var catalog = $('.catalog');
+var loader = $('.loader');
+var goodsCont = $('.content .goods');
+var cartMini = $('.basket');
+var popup = $('.popup_wrap');
+var productURL = 'JSON/groups.json';
+
+function genProductList(url){
+  $.getJSON(url, function(data){
+  var items = [];
+   $.each(data, function(key, val){
+    items.push('<li id="' + key + '">' + val + '</li>');
+  });
+ 
+  $('<ul/>', {
+    'class': 'my-new-list',
+     html: items.join('')
+  }).appendTo(catalog);
+});
+
+}
+genProductList(productURL);
+
+
