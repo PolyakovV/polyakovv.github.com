898acb0269ead8afe68742199db455703c4eaadf
PolyakovV <Valik@nm.ru>
2013-10-24 12:18:51 +0300

    Update

--- MyPortfolioScripts/shop/js/jquery.shop.js
+++ MyPortfolioScripts/shop/js/jquery.shop.js
@@ -1,25 +1,40 @@
 
-var catalog = $('.catalog');
+var catalogCont = $('.catalog');
 var loader = $('.loader');
 var goodsCont = $('.content .goods');
 var cartMini = $('.basket');
 var popup = $('.popup_wrap');
 var productURL = 'JSON/groups.json';
 
-function genProductList(url){
-  $.getJSON(url, function(data){
-  var items = [];
-   $.each(data, function(key, val){
-    items.push('<li id="' + key + '">' + val + '</li>');
-  });
- 
-  $('<ul/>', {
-    'class': 'my-new-list',
-     html: items.join('')
-  }).appendTo(catalog);
-});
-console.log(url);
+function openGroupsItems(event) 
+{ 
+console.log(event.target.className);
+};
+
+$(document).on('groupsReady', function(){
+      console.log('readyGroups');
+        $('.groups li').on('click', openGroupsItems);
+    }
+    );
+
+function genGropsList(url) { // получение списка групп
+    $.getJSON(url, function(data) {
+        var items = [];
+        $.each(data, function(key, val) { // формирование списка
+            items.push('<li groupId="' + key + '" class=' + val.class + '>' + val.name + '</li>');
+        });
+
+        $('<ul/>', {
+            'class': 'groups',
+            html: items.join('')
+        }).appendTo(catalogCont);
+        $(document).trigger('groupsReady');
+    });
+
 }
-genProductList(productURL);
+genGropsList(productURL);
+
+
+
 
 
